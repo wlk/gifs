9d0d2d584e40dceb4c3300b1d471a16f0d406a25
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-01-17 17:12:20 +0100

    adding, displaying favourites working

--- app/pages/favourites/favourites.js
+++ app/pages/favourites/favourites.js
@@ -1,25 +1,45 @@
-import {IonicApp, Page, NavController, NavParams} from 'ionic/ionic';
-import {Http} from 'angular2/http';
-import 'rxjs/add/operator/map';
+import {IonicApp, Page, NavController, NavParams, Storage, SqlStorage, Platform} from 'ionic/ionic';
+import {ItemDetailsPage} from '../item-details/item-details';
 
 @Page({
     templateUrl: 'build/pages/favourites/favourites.html'
 })
 export class FavouritesPage {
-    constructor(app:IonicApp, nav:NavController, navParams:NavParams, http:Http) {
+    constructor(app:IonicApp, nav:NavController, navParams:NavParams, platform:Platform) {
         this.nav = nav;
-        this.http = http;
+        this.platform = platform;
         this.gifs = null;
 
-        // If we navigated to this page, we will have an item available as a nav param
-        this.selectedItem = navParams.get('item');
+        this.platform.ready().then(() => {
+            this.storage = new Storage(SqlStorage);
+            this.storage.query("SELECT id, url, downsized_url, original_url, fixed_width_still_url FROM favourites").then((data) => {
+                    this.gifs = [];
+                    if (data.res.rows.length > 0) {
+                        for (var i = 0; i < data.res.rows.length; i++) {
+                            console.log("adding gif to display: " + JSON.stringify(data.res.rows.item(i)));
+                            this.gifs.push({
+                                id: data.res.rows.item(i).id,
+                                url: data.res.rows.item(i).url,
+                                images: {
+                                    downsized: {url: data.res.rows.item(i).downsized_url},
+                                    original: {url: data.res.rows.item(i).original_url},
+                                    fixed_width_still: {url: data.res.rows.item(i).fixed_width_still_url}
+                                }
+                            });
+                        }
+                    }
+                },
+                (error) => {
+                    console.log("ERROR -> " + JSON.stringify(error.err));
+                });
+        });
+    }
 
-        this.http.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC').map(res => res.json()).subscribe(data => {
-                console.log("gifs loaded");
-                this.gifs = data.data.children;
-            },
-            err => {
-                console.log("Oops!");
-            });
+    displayDetails(gif) {
+        this.nav.push(ItemDetailsPage, gif);
+    }
+
+    shouldDisplayError() {
+        return !(typeof this.gifs !== 'undefined' && this.gifs !== null && this.gifs.hasOwnProperty('length') && this.gifs.length > 0)
     }
 }
--- app/pages/item-details/item-details.js
+++ app/pages/item-details/item-details.js
@@ -1,20 +1,48 @@
-import {Page, NavParams} from 'ionic/ionic';
+import {Page, NavParams, Storage, SqlStorage, Platform} from 'ionic/ionic';
 
 @Page({
     templateUrl: 'build/pages/item-details/item-details.html'
 })
 export class ItemDetailsPage {
-    constructor(navParams:NavParams) {
+    constructor(navParams:NavParams, platform:Platform) {
         this.navParams = navParams;
+        this.platform = platform;
         this.gif = navParams.data;
+
+        this.platform.ready().then(() => {
+            this.storage = new Storage(SqlStorage);
+        });
+    }
+
+    isFavourite() {
+        this.platform.ready().then(() => {
+            console.log("is favourite");
+            this.storage.query("SELECT id FROM favourites WHERE id = '" + this.gif.id + "'").then((data) => {
+                    return data.res.rows.length > 0;
+                },
+                (error) => {
+                    console.log("ERROR -> " + JSON.stringify(error.err));
+                });
+        });
     }
 
     addToFavourites() {
-        console.log("adding gif to favourites");
+        this.platform.ready().then(() => {
+            console.log("adding gif to favourites");
+            this.storage.query("INSERT INTO favourites (id, url, downsized_url, original_url, fixed_width_still_url) " +
+                    "VALUES('" + this.gif.id + "', '" + this.gif.url + "','" + this.gif.images.downsized.url + "','" + this.gif.images.original.url + "','" + this.gif.images.fixed_width_still.url + "') ")
+                .then(
+                    (data) => {
+                        console.log(JSON.stringify(data.res));
+                    },
+                    (error) => {
+                        console.log("ERROR -> " + JSON.stringify(error.err));
+                    });
+        });
     }
 
     share() {
-        if(device.platform === "Android" || device.platform === "iOS") {
+        if (device.platform === "Android" || device.platform === "iOS") {
             window.plugins.socialsharing.share(this.gif.url);
         } else {
             window.plugins.socialsharing.share(this.gif.url, null, null, null);
