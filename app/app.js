6c085783b533ecd48d38273d97ae8df00026a2b1
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-01-25 11:12:42 +0100

    angular prod mode

--- app/app.js
+++ app/app.js
@@ -3,10 +3,11 @@ import {App, IonicApp, Platform, Storage, SqlStorage} from 'ionic/ionic';
 import {SearchPage} from './pages/search/search';
 import {MostPopularPage} from './pages/mostPopular/mostPopular';
 import {FavouritesPage} from './pages/favourites/favourites';
-
+import { enableProdMode } from 'angular2/core';
 import {Giphy} from './providers/giphy';
 import {FavouritesDB} from './providers/favouritesDB';
 import {Ads} from './providers/ads';
+enableProdMode();
 
 @App({
     templateUrl: 'build/app.html',
